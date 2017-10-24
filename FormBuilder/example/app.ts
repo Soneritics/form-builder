ef357cfab8b55fc7c84942c72740bdc7e802f7bb
Jordi Jolink <info@soneritics.nl>
2017-10-24 18:19:25 +0200

    Switch to TypeScript for the example source application

--- FormBuilder/example/app.ts
+++ FormBuilder/example/app.ts
@@ -1,7 +1,7 @@
-window.onload = function () {
+﻿window.onload = function () {
     var ui = new UI(document.getElementById('content'));
     var logger = new ConsoleLogger();
     var formbuilder = new FormBuilder(ui, logger);
+
     $('#button-text').on('click', function () { formbuilder.Add(new FormElementText()); });
 };
-//# sourceMappingURL=app.js.map
