577e1976befbab61943ea2c1eebc9a01a5e3e987
Jordi Jolink <info@soneritics.nl>
2017-10-24 21:41:08 +0200

    update the example code

--- FormBuilder/example/app.js
+++ FormBuilder/example/app.js
@@ -2,6 +2,15 @@ window.onload = function () {
     var ui = new UI(document.getElementById('content'));
     var logger = new ConsoleLogger();
     var formbuilder = new FormBuilder(ui, logger);
+    // Buttons to create form elements
     $('#button-text').on('click', function () { formbuilder.Add(new FormElementText()); });
+    // Serialize/deserialize
+    $('#serialize').on('click', function () { $('#serialized').text(formbuilder.Export()); });
+    $('#deserialize').on('click', function () {
+        var formElements = [
+            new FormElementText()
+        ];
+        formbuilder.Import(JSON.parse($('#deserialize-content').val()), formElements);
+    });
 };
 //# sourceMappingURL=app.js.map
--- FormBuilder/example/app.ts
+++ FormBuilder/example/app.ts
@@ -3,5 +3,15 @@
     var logger = new ConsoleLogger();
     var formbuilder = new FormBuilder(ui, logger);
 
+    // Buttons to create form elements
     $('#button-text').on('click', function () { formbuilder.Add(new FormElementText()); });
+
+    // Serialize/deserialize
+    $('#serialize').on('click', function () { $('#serialized').text(formbuilder.Export()); });
+    $('#deserialize').on('click', function () {
+        var formElements: AbstractFormElement[] = [
+            new FormElementText()
+        ];
+        formbuilder.Import(JSON.parse($('#deserialize-content').val()), formElements);
+    });
 };
