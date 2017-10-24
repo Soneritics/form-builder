02f112a8c686802a2e2652b178c66b4589b6f899
Jordi Jolink <info@soneritics.nl>
2017-10-24 16:46:09 +0200

    Add Eventhandler class

---
+++ FormBuilder/src/Components/EventHandler.ts
@@ -0,0 +1,31 @@
+﻿class EventHandler
+{
+    private _eventListeners: { [id: string]: Array<((data?: any) => void)> } = {};
+
+    public On(event: string, handler: (data?: any) => void): void
+    {
+        this.Off(event, handler);
+
+        if (this._eventListeners[event] === undefined || this._eventListeners[event] == null) {
+            this._eventListeners[event] = [];
+        }
+
+        this._eventListeners[event].push(handler);
+    }
+
+    public Off(event: string, handler: (data?: any) => void): void
+    {
+        if (this._eventListeners[event] != undefined && this._eventListeners[event] != null) {
+            this._eventListeners[event] = this._eventListeners[event].filter(e => e !== handler);
+        }
+    }
+
+    public Trigger(event: string, data?: any): void
+    {
+        if (this._eventListeners[event] !== undefined || this._eventListeners[event] != null && this._eventListeners[event].length > 0) {
+            for (var handler of this._eventListeners[event]) {
+                handler(data);
+            }
+        }
+    }
+}
