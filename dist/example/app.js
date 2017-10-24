accc459572635b5c096d6535e5c1d0e750bf72eb
Jordi Jolink <info@soneritics.nl>
2017-10-24 22:41:13 +0200

    Update dist example

--- dist/example/app.js
+++ dist/example/app.js
@@ -1,7 +1,13 @@
 ﻿window.onload = function () {
-    var ui = new UI(document.getElementById('container'));
+    var ui = new UI(document.getElementById('content'));
     var logger = new ConsoleLogger();
     var formbuilder = new FormBuilder(ui, logger);
-
     $('#button-text').on('click', function () { formbuilder.Add(new FormElementText()); });
+    $('#serialize').on('click', function () { $('#serialized').text(formbuilder.Export()); });
+    $('#deserialize').on('click', function () {
+        var formElements = [
+            new FormElementText()
+        ];
+        formbuilder.Import(JSON.parse($('#deserialize-content').val()), formElements);
+    });
 };
