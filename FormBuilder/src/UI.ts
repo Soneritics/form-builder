86cbf7ba39bfab1e4f78ea5e2ae156643559b48f
Jordi Jolink <info@soneritics.nl>
2017-10-24 21:53:45 +0200

    Update the graphical interface

--- FormBuilder/src/UI.ts
+++ FormBuilder/src/UI.ts
@@ -25,14 +25,14 @@
         var row = $('<div class="row draggable"><div class="container"><div class="row wysiwyg"></div><div class="row options"></div></div></div>');
 
         if (element.HasLabel) {
-            $(row).find('.wysiwyg').html('<div class="col-sm-5"><div class="row"><div class="col-1 toggler"></div><div class="col label"></div></div></div><div class="col-sm-7 value"></div>');
+            $(row).find('.wysiwyg').html('<div class="col-4"><div class="row"><div class="col-1 toggler"></div><div class="col label"></div></div></div><div class="col-sm-7 value"></div>');
             $(row).find('.label').text(element.Label);
         } else {
             $(row).find('.wysiwyg').html('<div class="col-1 toggler"></div><div class="col value"></div>');
         }
 
         $(row).find('.value').html(element.getValueHtml());
-        $(row).find('.toggler').html('<a href="javascript:;">T</a>').find('a').on('click', function () {
+        $(row).find('.toggler').html('<a href="javascript:;"><i class="fa fa-arrow-circle-down"></i></a>').find('a').on('click', function () {
             var element = $(this);
             do {
                 element = $(element).parent();
@@ -56,7 +56,7 @@
         var values = element.Serialize();
         var result = '<div class="container">';
         for (var property of properties) {
-            result += '<div class="row"><div class="col-5">' + property.Label + '</div><div class="col">';
+            result += '<div class="row"><div class="col-4">' + property.Label + '</div><div class="col">';
 
             switch (property.Component) {
                 case 'text':
