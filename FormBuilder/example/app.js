78bd307dd90fd63fc960e4ac9e023249407acf93
Jordi Jolink <info@soneritics.nl>
2017-10-24 23:28:13 +0200

    Add FormElementTextArea

--- FormBuilder/example/app.js
+++ FormBuilder/example/app.js
@@ -5,12 +5,14 @@ window.onload = function () {
     // Buttons to create form elements
     $('#button-text').on('click', function () { formbuilder.Add(new FormElementText()); });
     $('#button-textinput').on('click', function () { formbuilder.Add(new FormElementTextInput()); });
+    $('#button-textarea').on('click', function () { formbuilder.Add(new FormElementTextArea()); });
     // Serialize/deserialize
     $('#serialize').on('click', function () { $('#serialized').text(formbuilder.Export()); });
     $('#deserialize').on('click', function () {
         var formElements = [
             new FormElementText(),
-            new FormElementTextInput()
+            new FormElementTextInput(),
+            new FormElementTextArea()
         ];
         formbuilder.Import(JSON.parse($('#deserialize-content').val()), formElements);
     });
---
+++ FormBuilder/src/Elements/FormElementTextArea.ts
@@ -0,0 +1,42 @@
+﻿class FormElementTextArea extends AbstractFormElement
+{
+    public Type: string = 'FormElementTextInput';
+    public Name: string;
+    public Properties: ElementProperties[] = [
+        new ElementProperties('Placeholder', 'Placeholder', 'text')
+    ];
+    public Placeholder: string = "";
+
+    public CreateAndBindDisplayValue()
+    {
+        this._binding.html('<textarea></textarea>');
+        this._binding.find('textarea').attr('placeholder', this.Placeholder);
+        return this._binding;
+    }
+
+    public Serialize(): { [id: string]: string }
+    {
+        return {
+            Type: this.Type,
+            Name: this.Name,
+            Label: this.Label,
+            Placeholder: this.Placeholder
+        };
+    }
+
+    public Deserialize(data: { [id: string]: string }): void
+    {
+        if (data['Name'] !== undefined) {
+            this.Name = data['Name'];
+        }
+
+        if (data['Label'] !== undefined) {
+            this.Label = data['Label'];
+        }
+
+        if (data['Placeholder'] !== undefined) {
+            this.Placeholder = data['Placeholder'];
+        }
+    }
+
+}
